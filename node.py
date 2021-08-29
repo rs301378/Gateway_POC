a5a18035abc17fc75e388aaec454837d0a2ce2ab
Pratyush Verma <2019eem1010@iitrpr.ac.in>
2021-08-29 19:18:56 +0530

    BLE Gateway

--- node.py
+++ node.py
@@ -1,8 +1,26 @@
 import json
-from bluepy.btle import Scanner
+from bluepy.btle import Scanner, DefaultDelegate , UUID, Peripheral
+from time import sleep
+import struct
+import sys
 
 def hextodec(value):
     return -(value & 0x8000) | (value & 0x7fff)
+    
+def LEDconfig(mac,config):
+    led_service_uuid = UUID("00001523-1212-efde-1523-785feabcd123")
+    led_char_uuid = UUID("00001525-1212-efde-1523-785feabcd123")
+    
+    #put if statement for scan clash and exception
+    if SCAN_STATUS=='Inactive'
+        p = Peripheral(mac, "random")
+        led_srv=p.getServiceByUUID(led_service_uuid) # Service object for dev
+        led_ch=led_srv.getCharacteristics(led_char_uuid)[0] # Charateristic object for dev
+        if config=='True':
+            led_ch.write(struct.pack('B', 0x01))
+        if config=='False':
+            led_ch.write(struct.pack('B', 0x00))
+        p.disconnect()
 
 def app_node(db):
 
@@ -11,19 +29,28 @@ def app_node(db):
     global BT_STATUS
     while True:
         if N_STATUS=='Active':
+            #Accelerometer Section
+            #with open("/sys/devices/virtual/misc/FreescaleAccelerometer/data","r") as VAL:
+            #   r=str(VAL.readline())
+            #BLE Section
             bt=subprocess.check_output(['hciconfig'])
             if b'UP' in bt:
                 BT_STATUS='Active'
             else:
                 BT_STATUS='Inactive'
             if BT_STATUS=='Active' and C_STATUS=='Active':
+                SCAN_STATUS='Active'
                 lescan=Scanner(0)
                 devices=lescan.scan(3)
                 for dev in devices:
                     man=dev.getValueText(255)
+                    #print(man)
+                    #print(len(man))
                     z=man[14:16] + man[12:14]
                     y=man[10:12] + man[8:10]
                     x=man[6:8] + man[4:6]
+                    #print(x,y,z)
+                    #print(hextodec(int(x, 16))*0.00245,hextodec(int(y, 16))*0.00245,hextodec(int(z, 16))*0.00245)
                     x=hextodec(int(x, 16))*0.00245
 				    y=hextodec(int(y, 16))*0.00245
 				    z=hextodec(int(z, 16))*0.00245
@@ -33,5 +60,8 @@ def app_node(db):
                         payload.update({desc:value})
                     if not q.full() and C_STATUS=='Active':
                         q.put(payload,block=True,timeout=2)
+                SCAN_STATUS='Inactive'
+            #elif not q.full() and C_STATUS=='Active' and BT_STATUS=='Inactive':
+            #    q.put(r,block=True,timeout=2)
 
         time.sleep(3)
